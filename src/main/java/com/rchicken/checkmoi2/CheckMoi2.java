538c997eccfd14fb012ba606a73c3eb886781fbd
kimyo <41179265+kimziou77@users.noreply.github.com>
2022-10-17 19:03:20 +0900

    init: project init (#7)

---
+++ src/main/java/com/rchicken/checkmoi2/CheckMoi2.java
@@ -0,0 +1,13 @@
+package com.rchicken.checkmoi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckMoi2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckMoi2.class, args);
+    }
+
+}
