c3ec12de34dba3e420b59cb5962d6e735b3de5e7
kimziou77 <kimziou77@naver.com>
2022-10-23 00:02:54 +0900

    refac: build 오류 수정

--- src/main/java/com/rchicken/checkmoi2/global/HealthApi.java
+++ src/main/java/com/rchicken/checkmoi2/global/HealthApi.java
@@ -1,15 +1,15 @@
 package com.rchicken.checkmoi2.global;
 
-import com.rchicken.checkmoi2.global.model.SuccessResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rchicken.checkmoi2.global.model.SuccessResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "헬스체크", description = "헬스체크 API")
 @RestController
@@ -20,7 +20,7 @@ public class HealthApi {
     @Operation(summary = "healthy", description = "어플리케이션의 실행 상태를 확인하는 API")
     @GetMapping
     public ResponseEntity<SuccessResponse<String>> healthy() {
-        return ResponseEntity.ok(new SuccessResponse<>("CheckMoi v2", LocalDateTime.now()));
+        return ResponseEntity.ok(SuccessResponse.of("CheckMoi v2"));
     }
 
 }
--- src/main/java/com/rchicken/checkmoi2/global/model/SuccessResponse.java
+++ src/main/java/com/rchicken/checkmoi2/global/model/SuccessResponse.java
@@ -1,10 +1,10 @@
 package com.rchicken.checkmoi2.global.model;
 
-
 import java.time.LocalDateTime;
 
 public record SuccessResponse<T>(T data, LocalDateTime time) {
-public static SuccessResponse<T> of(T data){
-    return new SuccessResponse<>(data, LocalDateTime.now());
-}
+
+    public static <T> SuccessResponse<T> of(T data) {
+        return new SuccessResponse<>(data, LocalDateTime.now());
+    }
 }
