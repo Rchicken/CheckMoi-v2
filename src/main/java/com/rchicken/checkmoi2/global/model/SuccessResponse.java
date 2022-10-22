a61cb300932d9d747c290cd0bf2d28d40c2e156c
zxcv9203 <41960243+zxcv9203@users.noreply.github.com>
2022-10-22 22:46:58 +0900

    fix : 응답 객체에 정적 팩토리 메서드 패턴 적용
    Co-authored-by: kimyo <41179265+kimziou77@users.noreply.github.com>

--- src/main/java/com/rchicken/checkmoi2/global/model/SuccessResponse.java
+++ src/main/java/com/rchicken/checkmoi2/global/model/SuccessResponse.java
@@ -4,5 +4,7 @@ package com.rchicken.checkmoi2.global.model;
 import java.time.LocalDateTime;
 
 public record SuccessResponse<T>(T data, LocalDateTime time) {
-
+public static SuccessResponse<T> of(T data){
+    return new SuccessResponse<>(data, LocalDateTime.now());
+}
 }
